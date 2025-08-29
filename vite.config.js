335ccafa1f4233a208e5a22cfe144250f1dac1ab
Oliver Stamp <oastamp08@gmail.com>
2025-08-29 02:01:19 -0300

    testing vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  basee: '/personal-site-public/',
   plugins: [react(), tailwindcss()],
 })
